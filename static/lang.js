a67a6b6c2d13cb6e3b2a295d02c3c6cf18ace3e1
sunecolo <76092687+sunecolo@users.noreply.github.com>
2020-12-16 16:35:16 +0800

    Add files via upload

---
+++ static/lang.js
@@ -0,0 +1,115 @@
+var lang_json = {
+	"en":{
+		"tag-name":"YYY-20",
+		"tag-desc":"All functions are implemented using solidity",
+		"tag-send-eth":"Send",
+		"taga-title":"Global Data",
+		"taga-eths":"Maximum direct push ETH",
+		"taga-address":"Maximum address",
+		"taga-selfeths":"My direct push ETH",
+		"taga-will":"Referrals to be registered",
+		
+		
+		"tagb-title":"MY INCOME",
+		"tagb-time":"Next income countdown",
+		"tagb-gen":"Dividend income",
+		"tagb-dy":"Dynamic income",
+		"tagb-win":"Rewards",
+		"tagb-limit":"2.2-income limit remains",
+		"tagb-unset":"Unsettled income",
+		"tagb-set-action":"settle",
+		"tagb-unwithdraw":"Unwithdraw income",
+		"tagb-withdraw-action":"Withdraw",
+		
+		"tagc-title":"INVITE",
+		"tagc-link":"My invitation link",
+		"tagc-reffer":"My referrer",
+		"tagc-copy-action":"Copy invitation link",
+		
+		"tagd-title":"HISTORICAL REWARD",
+		"tagd-win":"Number of rewards",
+		"tagd-win-refs":"Maximum recommended eth",
+		"tagd-win-address":"Reward account",
+		
+		"tage-title":"STATISTICS",
+		"tage-t-gen":"Total divided income",
+		"tage-t-dy":"Total dynamic income",
+		"tage-t-join":"Total join eth"
+	},
+	"zh":{
+		"tag-name":"YYY-20",
+		"tag-desc":"所有功能均采用Solidity实现",
+		"tag-send-eth":"发送",
+		"taga-title":"全局数据",
+		"taga-eths":"本轮最大直推ETH",
+		"taga-address":"本轮最大直推账户",
+		"taga-selfeths":"我本轮直推ETH",
+		"taga-will":"即将注册的推荐人",
+		
+		
+		"tagb-title":"我的收益",
+		"tagb-time":"下一次收益时间",
+		"tagb-gen":"分红收益",
+		"tagb-dy":"动态收益",
+		"tagb-win":"奖金收益",
+		"tagb-limit":"2.2倍限制剩余",
+		"tagb-unset":"未结算收益",
+		"tagb-set-action":"结算",
+		"tagb-unwithdraw":"未提现收益",
+		"tagb-withdraw-action":"提现",
+		
+		"tagc-title":"邀请",
+		"tagc-link":"我的邀请链接",
+		"tagc-reffer":"我的推荐人",
+		"tagc-copy-action":"复制邀请链接",
+		
+		"tagd-title":"历史奖励",
+		"tagd-win":"奖励数量",
+		"tagd-win-refs":"最大推荐的以太数",
+		"tagd-win-address":"奖励账户",
+		
+		"tage-title":"统计",
+		"tage-t-gen":"总分红",
+		"tage-t-dy":"总动态",
+		"tage-t-join":"总参与"
+	},
+	"jp":{
+		"tag-name":"YYY-20",
+		"tag-desc":"すべての機能はSolidityで実装されています",
+		"tag-send-eth":"送る",
+		"taga-title":"グローバルデータ",
+		"taga-eths":"最大ダイレクトプッシュETH",
+		"taga-address":"最大推奨アカウント",
+		"taga-selfeths":"私のダイレクトプッシュETH",
+		"taga-will":"おすすめの紹介者",
+		
+		
+		"tagb-title":"私の収入",
+		"tagb-time":"次の収益時間",
+		"tagb-gen":"配当収入",
+		"tagb-dy":"紹介収入",
+		"tagb-win":"ボーナス収入",
+		"tagb-limit":"残り2.2回",
+		"tagb-unset":"未解決の収益",
+		"tagb-set-action":"決済",
+		"tagb-unwithdraw":"未払いの収益",
+		"tagb-withdraw-action":"エキス",
+		
+		"tagc-title":"お勧め",
+		"tagc-link":"私の紹介リンク",
+		"tagc-reffer":"私の紹介者",
+		"tagc-copy-action":"紹介リンクをコピー",
+		
+		"tagd-title":"歴史的褒賞",
+		"tagd-win":"報酬の数",
+		"tagd-win-refs":"最大紹介ETH",
+		"tagd-win-address":"特典アカウント",
+		
+		"tage-title":"統計",
+		"tage-t-gen":"総配当",
+		"tage-t-dy":"総紹介報酬",
+		"tage-t-join":"総参加"
+	}
+	
+	
+	};
